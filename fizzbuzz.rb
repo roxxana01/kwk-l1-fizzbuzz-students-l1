304f1f0473a0fef6d5aae7a0fc32c3f14ffe5847
persia sarram <kittycuppers01@gmail.com>
2018-06-05 19:23:25 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
    puts "Fizz" # Go fizz
-   if int % 3 == 0 # if the number int is divisible by 3
+   if int % 5 == 0 # if the number int is divisible by 5
+   puts "Buzz"
   end
 end
  
