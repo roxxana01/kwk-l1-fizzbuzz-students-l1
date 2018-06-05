81a583d4a062b8f1eb8d43f37c6036318c4f8516
persia sarram <kittycuppers01@gmail.com>
2018-06-05 19:26:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(int)
+  int = 45
   if int % 3 == 0 # if the number int is divisible by 3
    puts "Fizz" # Go fizz
    elsif int % 5 == 0 # if the number int is divisible by 5
    puts "Buzz"
+    elsif int % 7 == 0 # if the number int is divisible by 7
+    puts "Fizzbuzz"
    
   end
 end
