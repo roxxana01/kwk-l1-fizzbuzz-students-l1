469ac76ef650fe915700d41fc7b7886cc8c7804c
persia sarram <kittycuppers01@gmail.com>
2018-06-05 19:34:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,5 +10,5 @@ def fizzbuzz(int)
 end
  
 fizzbuzz(105) # => You should see a return of "Fizz"
-fizzbuzz(5) # => You should see a return of nil
+
 # fizzbuzz()  # => You should get an ArgumentError
